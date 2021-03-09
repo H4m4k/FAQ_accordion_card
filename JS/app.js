d18a3967f12bc91a1f17b6a88c4fd86708cf5fff
H4m4k <pawel.janik.1983@gmail.com>
2021-03-09 15:22:32 +0100

    JS fixes - changed the source point of determining when to rotate the red arrow, no more does clicking other questions break the functionality

--- JS/app.js
+++ JS/app.js
@@ -2,17 +2,12 @@ const faq = document.querySelectorAll('.list');
 
 faq.forEach( list => list.addEventListener('click', toggle))
 
-let rotate = 0;
-
 function toggle (event) {
+    let rotate = event.target.children[0].style.rotate || '0deg';
+
     event.target.classList.toggle('addWeight');
     event.target.nextElementSibling.classList.toggle('off');
     
-    if(rotate === 0) {
-        event.target.children[0].style.rotate = '180deg'; 
-        return rotate = 180;
-    } 
-
-    event.target.children[0].style.rotate = '0deg'
-    return rotate = 0;
+    if(rotate === '0deg') return event.target.children[0].style.rotate = '180deg'; 
+    return event.target.children[0].style.rotate = '0deg'
 }
