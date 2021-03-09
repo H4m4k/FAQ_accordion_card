89deacad643dcaed8e7fb10a5077306bca190760
H4m4k <pawel.janik.1983@gmail.com>
2021-03-09 11:20:02 +0100

    JS functionality done and working properly, solved the whitespace-click bug

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,18 @@
-const faq = document.querySelector('.list');
+const faq = document.querySelectorAll('.list');
 
-faq.addEventListener('click', toggle);
+faq.forEach( list => list.addEventListener('click', toggle))
+
+let rotate = 0;
 
 function toggle (event) {
+    event.target.classList.toggle('addWeight');
     event.target.nextElementSibling.classList.toggle('off');
+    
+    if(rotate === 0) {
+        event.target.children[0].style.rotate = '180deg'; 
+        return rotate = 180;
+    } 
+
+    event.target.children[0].style.rotate = '0deg'
+    return rotate = 0;
 }
-
-
