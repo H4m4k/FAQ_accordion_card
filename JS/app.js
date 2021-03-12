d9f0e474ba37e98c5e526199cbd747e99bd01b42
H4m4k <pawel.janik.1983@gmail.com>
2021-03-12 18:00:35 +0100

    Error in HTML structure tags, fixed

--- JS/app.js
+++ JS/app.js
@@ -6,7 +6,7 @@ function toggle (event) {
     let rotate = event.target.children[0].style.rotate || '0deg';
 
     event.target.classList.toggle('addWeight');
-    event.target.nextElementSibling.classList.toggle('off');
+    // event.target.nextElementSibling.classList.toggle('off');
     
     if(rotate === '0deg') return event.target.children[0].style.rotate = '180deg'; 
     return event.target.children[0].style.rotate = '0deg'
